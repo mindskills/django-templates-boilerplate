d5300c310ff70bca9540d4f20ce84f9ce4933560
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-02-02 15:47:59 +0300

    udpate user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
-    pass
+
+    is_agree_to_processing_personal_data = models.BooleanField(_('Согласен на обработку перс. данных'), default=False)
+    is_confirm_phone = models.BooleanField(_('Подтвердил номер телефона'), default=False)
+    is_confirm_email = models.BooleanField(_('Подтвердил адрес эл. почты'), default=False)
+    phone = models.CharField(_('Номер телефона'), max_length=16, null=True, blank=True)
+
+    class Meta:
+        db_table = 'auth_user'
